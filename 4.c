df4fe7ec32188218977c5c3ffbc7acdc05a05d49
Sam7202 <nk98048@gmail.com>
2024-04-02 00:30:06 +0800

    work, but need to add some comment & make it cleaner

--- 4.c
+++ 4.c
@@ -28,20 +28,28 @@ int main()
     	//loop1
     	"loop1:\n\t"
     	"add %[j], %[x0],%[x0]\n\t"	//reset j to new loop2
+
     	
-    	"add %[p_x], %[x0], %[x_0]\n\t" //p_x = &x[0]
+    	"mv %[p_x], %[x_0]\n\t" //p_x = &x[0]
     	/* do matrix multiply */
     	//loop2
     	"loop2:\n\t"
     	"add %[f], %[x0],%[x0]\n\t"	//reset f to new loop3
+    	"lw a5, 0(%[p_y])\n\t"//load *p_y to a5
     	
     	"mul %[temp1], %[i], %[num12]\n\t" //temp = i * 3
     	"add %[p_h], %[temp1], %[h0]\n\t" //p_h = temp + &h[0](may need *12)
     	
     	//loop3
     	"loop3:\n\t"
-    	"mul %[temp1], %[h_i], %[x_i]\n\t"//temp1 = *p_h * *p_x	
-        "add %[y_i], %[y_i], %[temp1]\n\t"//*p_y =*p_y + temp1
+    	
+    	"lw a6, 0(%[p_h])\n\t"// load *p_h
+
+    	"lw a7, 0(%[p_x])\n\t"// load *p_x
+
+    	"mul a6, a6, a7\n\t"//temp1 = *p_h * *p_x	
+
+        "add a5, a5, a6\n\t"//*p_y =*p_y + temp1
         "addi %[p_h], %[p_h], 4\n\t"//p_h++
         "addi %[p_x], %[p_x], 8\n\t"//p_x+=8(1->3->5)
     	//loop3 end
@@ -49,7 +57,8 @@ int main()
     	"blt %[f], %[bound3], loop3\n\t"// back to loop3
     	
     	/* next element */
-    	"add %[p_x] , %[x0], %[x_1]\n\t"//p_x = &x[1]
+    	"sw a5, 0(%[p_y])\n\t"
+    	"mv %[p_x], %[x_1]\n\t"//p_x = &x[1]
     	"addi %[p_y] , %[p_y], 4\n\t"//p_y++
     	//loop2 end
     	"addi %[j], %[j], 1\n\t"	//j++
@@ -60,7 +69,7 @@ int main()
     	"Exit:\n\t"
     	:[i]"+r"(i1), [j]"+r"(j), [f]"+r"(f), [p_x]"+r"(p_x), [p_y]"+r"(p_y), [p_h]"+r"(p_h), [temp1]"+r"(temp1), [y_i]"=r"(*p_y)
     	:[x_0]"r"(&x[0]), [bound3]"r"(bound_3), [bound2]"r"(bound_2), [h0]"r"(&h[0]), [num12]"r"(num12), [x_1]"r"(&x[1]), [h_i]"r"(*p_h), [x_i]"r"(*p_x),[x0]"r"(0)//input
-    	
+    	:"a5","a6","a7"
     );
 
     p_y = &y[0];
